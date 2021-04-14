5d3b52edd38213f30c17a7039ab54e364cd1e59e
Brian Koo <comc49@gmail.com>
2021-04-14 14:09:13 -0400

    added return uri fix

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,31 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import awsExports from "../src/aws-exports";
+import awsconfig from "../src/aws-exports";
 import Amplify from "aws-amplify";
 import {AmplifyAuthenticator, AmplifySignOut} from "@aws-amplify/ui-react";
 import { Auth } from 'aws-amplify'
 
+if (process.browser) {
+  // Client-side-only code
+  const { host } = window.location;
+
+  // Fix issues with multiple redirect urls.
+  // Try to figure out which one to use...
+  if (awsconfig.oauth.redirectSignIn.includes(',')) {
+    const filterHost = url => new URL(url).host === host;
+    awsconfig.oauth.redirectSignIn = awsconfig.oauth.redirectSignIn
+      .split(',')
+      .filter(filterHost)
+      .shift();
+      awsconfig.oauth.redirectSignOut = awsconfig.oauth.redirectSignOut
+      .split(',')
+      .filter(filterHost)
+      .shift();
+  }
+}
 
 
-Amplify.configure({...awsExports, ssr: true });
+Amplify.configure({...awsconfig, ssr: true });
 
 
 export default function Home() {
